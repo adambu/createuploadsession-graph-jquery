d2922b5dd8735f507d6f5ef9109136b895002302
Adam Burns <adam@newservices.com>
2018-01-29 18:46:52 -0800

    Removed SPDev specific comments and tenant specific data.

--- App/Scripts/config.js
+++ App/Scripts/config.js
@@ -1,5 +1,5 @@
 // If your account is 'yourdomain.onmicrosoft.com', this value should be 'yourdomain'.
-var tenant = 'adambu365';
+var tenant = 'yourTenantName';
 
 // Get this value from the Configure tab of your application page in Azure Management Portal.
-var clientId = 'fe949c42-436f-4fdc-b061-dc404d66b5c7';
+var clientId = '<<your-azureAD-Application-xxxx-xxxxxxxx>>';
